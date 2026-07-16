0fb9e586997b20041d780da55a73107b38038121
Raman-Khiva <ramandeep01167@gmail.com>
2026-07-16 19:03:06 +0000

    add doppler env

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -27,7 +27,7 @@ const Page = () => {
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-foreground opacity-75"></span>
           <span className="relative inline-flex h-2 w-2 rounded-full bg-foreground"></span>
         </span>
-        Welcome to Weavit
+        Welcome to Weavit<span>{process.env.NEXT_PUBLIC_VERSION || "v0"}</span>
       </div>
 
       {/* Tagline */}
