1278d68a0553db4a28d329663b0c1e6dc8ed8f47
Raman-Khiva <ramandeep01167@gmail.com>
2026-07-22 16:36:55 +0000

    integrate db

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express"
 import { clerkMiddleware, getAuth } from "@clerk/express"
-
+import {prisma} from '../src/config/db'
 import cors from "cors"
 
 const app = express()
@@ -15,6 +15,65 @@ app.get('/auth-status',(req:Request,res:Response)=>{
   const {isAuthenticated, userId} = getAuth(req);
   res.json({message:`user is ${isAuthenticated? "" : "not "}Authenticated`})
 })
+
+app.post('/syncUser',async (req:Request, res:Response)=>{
+   try {
+    console.log("Entering /syncUser")
+      const {userId} = await getAuth(req);
+      
+      let existingUser = true;
+      let user = await prisma.user.findUnique({
+        where : {clerkId : userId}
+      })
+
+      if(!user){
+        existingUser = false;
+        user = await prisma.user.create({
+          data:{
+            clerkId : userId,
+          }
+        })
+      }
+      
+      res.status(200).json({
+        message : existingUser?"Existing user found":"New user created successfully",
+        data : {
+          user : user 
+        }
+      })
+    
+   } catch (error) {
+     console.log("Error while syncing error",error);
+     res.status(500).json({
+        message: "failed to sync user",
+        error : error
+     })
+   }
+})
+
+
+app.post('/db-test',async (req:Request, res:Response)=>{
+    const {name} = req.body;
+    
+    try{
+    const createTest = await prisma.test.create({
+      data:{
+        name
+      }
+    })
+    res.status(201).json({
+      message:"Test create successfully",
+      createTest
+    })
+  }catch(err){
+    res.status(500).json({
+      message:"failed to create test",
+      error: err
+    })
+  }
+
+})
+
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from Express API" })
